5139ee5d548d21a204be9a2bc86c2e40de68b2e2
Andriiboichuk22 <150365808+Andriiboichuk22@users.noreply.github.com>
2025-03-24 11:04:35 +0200

    Update routes.py

--- lab1/books/routes.py
+++ lab1/books/routes.py
@@ -5,29 +5,34 @@ from .schema import BookSchema
 
 book_repo = BookRepository()
 
-@books_bp.route("/library", methods=["GET"])
+
+@books_bp.route("/books", methods=["GET"])
 def get_all_books():
     return jsonify(book_repo.get_books())
 
-@books_bp.route("/library/book/<int:book_id>", methods=["GET"])
+
+@books_bp.route("/book/<int:book_id>", methods=["GET"])
 def get_book_by_id(book_id):
     book = book_repo.get_book(book_id)
     if not book:
-        return jsonify({"message": "Дану книгу не знайдено"}), 404
-        return jsonify(book)
+        return jsonify({"message": "Book not found"}), 404
+    return jsonify(book)
 
-@books_bp.route("/library/book/<int:book_id>", methods=["DELETE"])
+
+@books_bp.route("/book/<int:book_id>", methods=["DELETE"])
 def delete_book_by_id(book_id):
     if book_repo.delete_book(book_id):
-        return jsonify({"message": "Дану книгу видалено успішно"}), 204
-        return jsonify({"message": "Дану книгу не знайдено"}), 404
+        return jsonify({"message": "Book deleted successfully"}), 204
+    return jsonify({"message": "Book not found"}), 404
 
-@books_bp.route("/library/add-book", methods=["POST"])
+
+@books_bp.route("/books", methods=["POST"])
 def create_book():
     schema = BookSchema()
     try:
         book_data = schema.load(request.get_json())
     except Exception as e:
-            return jsonify({"message": str(e)}), 400
-            new_book = book_repo.add_book(book_data)
-            return jsonify(new_book), 201
+        return jsonify({"message": str(e)}), 400
+
+    new_book = book_repo.add_book(book_data)
+    return jsonify(new_book), 201
